63c5403af2ce3d6c7fce23f06fe0b192944c5e06
Amanda Oliveira <amandaol7b@gmail.com>
2025-03-16 16:42:07 -0300

    feat: inicialição do projeto e criação das rotas para votação interna e publica

---
+++ src/app/votacao-publica/page.js
@@ -0,0 +1,8 @@
+export default function VotacaoPublica() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Página de Votação Pública</h1>
+      <p>Bem-vindo à página de votação publica.</p>
+    </div>
+  );
+}
