fcf289484d1895131bb6dc1e7f9ad20f3975e8a6
Eliharison Gabriel <124097279+Eliharison@users.noreply.github.com>
2025-03-19 16:46:36 +0000

    refactor: reestruturando rotas do Front-End seguindo convenção do `Notion`

--- src/app/votacao-interna/page.js
+++
@@ -1,8 +0,0 @@
-export default function VotacaoInterna() {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>Página de Votação Interna</h1>
-      <p>Bem-vindo à página de votação interna.</p>
-    </div>
-  );
-}
