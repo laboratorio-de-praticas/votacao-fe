7d9ad1ee280c126ed0c66f3b495a96c74f54e999
Leandro Muniz <160612304+Leandr0-August0@users.noreply.github.com>
2025-03-27 10:55:14 -0300

    fix: adicionando o alt do ícone de imagem para sumir o erro apresentado pelo ESlint
    Co-authored-by: Amanda Oliveira <100137341+amandazzoc@users.noreply.github.com>

--- src/app/votacao/publica/convidados/page.js
+++ src/app/votacao/publica/convidados/page.js
@@ -24,6 +24,7 @@ export default function VotacaoPublica() {
                                 <Image
                                     size={50}
                                     className="justify-self-center stroke-white"
+                                    alt="Imagem do Projeto"
                                 />
                                 {/* <img src="" alt="" /> */}
                             </div>
