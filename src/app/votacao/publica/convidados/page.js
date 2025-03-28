4fbcb5bf62ce2f647c60c66cf78ae80d5967d00d
Amanda Oliveira <amandaol7b@gmail.com>
2025-03-28 19:40:28 -0300

    feat: adição da navbar em todas as paginas

--- src/app/votacao/publica/convidados/page.js
+++ src/app/votacao/publica/convidados/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 import { Image } from 'lucide-react';
