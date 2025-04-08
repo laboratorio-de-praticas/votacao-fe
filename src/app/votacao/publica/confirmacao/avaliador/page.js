d2cd50fb051e6694aeb521911c9813b3bc893537
Amanda Oliveira <amandaol7b@gmail.com>
2025-04-08 19:44:15 -0300

    feat: ajuste na visualização do botão em telas mobile

--- src/app/votacao/publica/confirmacao/avaliador/page.js
+++ src/app/votacao/publica/confirmacao/avaliador/page.js
@@ -3,6 +3,7 @@ import Header from "@/components/header";
 import { useRouter } from "next/navigation";
 import CongratsText from "@/components/congratsText";
 import ProjectCard from "@/components/projectCard";
+import Button from "@/components/button";
 
 export default function VotacaoPublica() {
   const router = useRouter();
@@ -17,16 +18,19 @@ export default function VotacaoPublica() {
     <>
       <Header text={"PROJETO"} />
       <div className="flex flex-col md:flex-row md:w-5/6 gap-8 mt-6 md:gap-12 md:mt-16 md:mb-16 md:justify-stretch md:self-center">
-        <ProjectCard
-          projectName={"Nome do Projeto"}
-          projectDescription={"Descrição do Projeto"}
-          imageUrl={"/undefinedImage.svg"}
-        />
         <CongratsText
           text={description}
           onClickItem={handleRedirect}
           textButton={"VOTAR"}
         />
+        <ProjectCard
+          projectName={"Nome do Projeto"}
+          projectDescription={"Descrição do Projeto"}
+          imageUrl={"/undefinedImage.svg"}
+        />
+        <div className="md:hidden w-full flex justify-center">
+          <Button onClick={handleRedirect} text={"VOTAR"} />
+        </div>
       </div>
     </>
   );
