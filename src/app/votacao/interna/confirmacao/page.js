c150053b20beb80ea1e166cd3fd70eb08ccef02f
Amanda Oliveira <amandaol7b@gmail.com>
2025-04-14 20:14:53 -0300

    feat: Atualização do front de acordo com o figma

--- src/app/votacao/interna/confirmacao/page.js
+++ src/app/votacao/interna/confirmacao/page.js
@@ -4,9 +4,9 @@ import React, { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import ConfirmModal from "@/components/confirmModal";
 import Header from "@/components/header";
-import CongratsText from "@/components/congratsText";
 import CandidateCard from "@/components/candidateCard";
 import { useSearchParams } from "next/navigation";
+import Button from "@/components/button";
 
 const ConfirmationPage = () => {
   const router = useRouter();
@@ -88,26 +88,26 @@ const ConfirmationPage = () => {
     }
   };
 
-  const description =
-    "verifique abaixo se o projeto é o desejado e confirme em seguida.";
-
   return (
     <>
       <Header text={"REPRESENTANTES"} />
-      <div className="flex flex-col md:flex-row md:w-5/6 gap-8 mt-6 md:gap-12 md:mt-16 md:mb-16 md:justify-stretch md:self-center">
-        <div className=" md:order-1 md:col-start-1 md:row-span-2 flex justify-center">
-          <CandidateCard
-            mobileImage="/placeholder_mobile.png"
-            image="/placeholder_desktop.png"
-            name="Teste da Silva"
-            email="teste@fatec.sp.gov.br"
-            room="DSM 3"
-          />
-        </div>
-        <CongratsText
-          text={description}
-          onClickItem={handleConfirm}
-          textButton={status === null ? "CARREGANDO..." : status ? "CONFIRMAR VOTO" : "VOTO REGISTRADO"}
+      <div className="flex flex-col justify-center items-center gap-8 mt-16 md:mb-16">
+        <CandidateCard
+          mobileImage="/placeholder_mobile.png"
+          image="/placeholder_desktop.png"
+          name="Teste da Silva"
+          email="teste@fatec.sp.gov.br"
+          room="DSM 3"
+        />
+        <Button
+          onClick={handleConfirm}
+          text={
+            status === null
+              ? "CARREGANDO..."
+              : status
+              ? "VOTAR"
+              : "VOTO REGISTRADO"
+          }
           status={status}
         />
       </div>
